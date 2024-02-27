5b6d53ea2dee2067c8ec376235b406fabd284790
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-27 12:53:20 +0900

    プロジェクト追加してSPMでLottieを追加

---
+++ LottiePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottiePractice
+//
+//  Created by 澤木柊斗 on 2024/02/27.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
