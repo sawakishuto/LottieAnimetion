52dd4de79ee4ef87b3198106367b1fefae1c17d1
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-27 14:36:33 +0900

    collectionViewを定義

---
+++ LottiePractice/LottieCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LottieCollectionViewCell.swift
+//  LottiePractice
+//
+//  Created by 澤木柊斗 on 2024/02/27.
+//
+
+import UIKit
+
+class LottieCollectionViewCell: UICollectionViewCell {
+    
+}
--- LottiePractice/ViewController.swift
+++ LottiePractice/ViewController.swift
@@ -10,6 +10,20 @@ import Lottie
 
 class ViewController: UIViewController {
 
+let  screenSize: CGSize = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+final class ViewController: UIViewController {
+    private let collectionView: UICollectionView = {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        let collectionView: UICollectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height), collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        return collectionView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
