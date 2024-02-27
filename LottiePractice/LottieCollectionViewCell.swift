4e344134e684505319ee8fef3370ea6d61c31fb1
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-27 14:38:13 +0900

    collectionViewCellを追加

--- LottiePractice/LottieCollectionViewCell.swift
+++ LottiePractice/LottieCollectionViewCell.swift
@@ -8,5 +8,11 @@
 import UIKit
 
 class LottieCollectionViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
